4710154d22cd1f92be8fbb9a245593c202f02fc0
JonathanGzzBen <jonathangzzben@gmail.com>
2022-09-16 20:54:19 -0500

    Remove router import

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import {Router} from 'express'
 import morgan from 'morgan'
 import candidatesRoutes from './routes/candidates'
 import companiesRoutes from './routes/companies'
