ee21aa462f3db82abac39575f6d1c7865b0d8432
JonathanGzzBen <jonathangzzben@gmail.com>
2022-09-16 20:51:30 -0500

    Add jwt authentication

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import {Router} from 'express'
 import morgan from 'morgan'
 import candidatesRoutes from './routes/candidates'
 import companiesRoutes from './routes/companies'
@@ -9,6 +10,7 @@ import transportationsRoutes from './routes/transportations'
 import usersRoutes from './routes/users'
 import vacantsRoutes from './routes/vacants'
 
+const jwt = require('jsonwebtoken');
 const app = express();
 
 //Settings
@@ -18,6 +20,26 @@ app.set('port', process.env.PORT || 4000);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Authentication middleware
+const authenticateToken = (req: any, res: any, next: any) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null)
+        return res.sendStatus(401);
+
+    jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(403);
+        }
+
+        req.user = user;
+
+        next();
+    })
+}
 
 //Routes
 app.use('/api/candidates', candidatesRoutes);
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import app from './app'
 import { connection } from './database'
 import schedule from 'node-schedule'
 import { deleteAllCandidates } from './controllers/candidates.controller'
+import dotenv from 'dotenv'
 
 async function main() {
+    dotenv.config();
     connection();
     await app.listen(app.get('port'));
     console.log('Server on port', app.get('port'));
