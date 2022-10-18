b1195f2dd6abeaa3c21175402dd94f26be8ec366
greatooau <pajaroazulx@gmail.com>
2022-10-17 20:32:56 -0500

    CORS added

--- index.ts
+++ index.ts
@@ -3,13 +3,15 @@ import { connection } from './src/database'
 import schedule from 'node-schedule'
 import { deleteAllCandidates } from './src/controllers/candidates.controller'
 import dotenv from 'dotenv'
+import cors from 'cors';
 
 async function main() {
     dotenv.config();
-    connection();
-    await app.listen(app.get('port'));
+    await connection();
+    app.use(cors()); 
+    app.listen(app.get('port'));
     console.log('Server on port', app.get('port'));
-    const job = schedule.scheduleJob({hour: 23, minute: 59}, () => {
+    const job = schedule.scheduleJob({ hour: 23, minute: 59 }, () => {
         deleteAllCandidates();
     });
 }
