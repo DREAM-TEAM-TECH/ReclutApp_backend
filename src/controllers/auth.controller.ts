5bed3d1d97a901f0af7ee441a720fb4cd7f7753e
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-11-03 22:33:21 -0600

    Validation of the password and email in de db

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from 'express';
+import User from '../models/User'
 
-export const login = (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response) => {
+
   const { email, password } = req.body;
+
+  const user = await User.findOne({ email, password });
+
+  if (!user) {
+    return res.status(400).json({
+      msg: 'Email / Password no son correctos'
+    })
+  }
 
   res.json({ email, password });
 }
