0fdd708fac1e160484178b9eedbcc53a61841ebd
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-11-03 23:44:04 -0600

    Comparing the password sent in the login against db

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import bcryptjs from 'bcryptjs';
 import { generarJWT } from '../middlewares/generateJWT';
 import User from '../models/User'
 
@@ -6,11 +7,18 @@ export const login = async (req: Request, res: Response) => {
 
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email, password });
+  const user = await User.findOne({ email });
 
   if (!user) {
     return res.status(400).json({
       msg: 'Email / Password no son correctos'
+    })
+  }
+
+  const validPassword = bcryptjs.compareSync(password, user.password);
+  if (!validPassword) {
+    return res.status(400).json({
+      msg: 'Email / Password no son correctos.'
     })
   }
 
