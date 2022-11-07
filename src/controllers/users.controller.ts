db28badf3dc2cda35b8d8577656e7b158df0a26f
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-11-07 14:34:19 -0600

    Returning the token when creating the user

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,19 +1,22 @@
 import { Request, Response } from 'express'
 import bcryptjs from 'bcryptjs'
 import User from '../models/User'
+import { generarJWT } from '../middlewares/generateJWT';
 
 export async function createUser(req: Request, res: Response) {
 
-    const { password } = req.body;
+    const { password, email } = req.body;
 
     const salt = bcryptjs.genSaltSync();
     const passwordCrypt = bcryptjs.hashSync(password, salt);
-
     req.body.password = passwordCrypt;
+
+    const token = await generarJWT(email);
 
     const newUser = await User.create(req.body);
     return res.json({
         message: 'User created successfully',
+        token
     });
 }
 
