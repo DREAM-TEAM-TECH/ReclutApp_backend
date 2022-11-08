918522630bc41574d2a7fe7ba6f52123eb71e1df
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-11-07 23:02:47 -0600

    Populate the license_plates and points.id

--- src/controllers/routes.controller.ts
+++ src/controllers/routes.controller.ts
@@ -1,31 +1,33 @@
 import { Request, Response } from 'express'
 import Route from '../models/Route'
 
-export async function createRoute(req: Request, res: Response) { 
-    const newRoute = await Route.create(req.body); 
+export async function createRoute(req: Request, res: Response) {
+    const newRoute = await Route.create(req.body);
     return res.json({
         message: 'Route created successfully'
     });
 }
 
-export async function getRoutes(req: Request, res: Response) { 
-    const routes = await Route.find().populate('transportation', 'points');
+export async function getRoutes(req: Request, res: Response) {
+    const routes = await Route.find()
+        .populate('transportation', 'license_plates')
+        .populate('points.id', 'name');
     return res.json(routes);
 }
 
-export async function getRoute(req: Request, res: Response) { 
+export async function getRoute(req: Request, res: Response) {
     const route = await Route.findById(req.params.id).populate('transportation', 'points');
     return res.json(route);
 }
 
-export async function updateRoute(req: Request, res: Response) { 
+export async function updateRoute(req: Request, res: Response) {
     let route = await Route.findOneAndUpdate(req.params, req.body);
     return res.json({
         message: 'Route updated successfully'
     });
 }
 
-export async function deleteRoute(req: Request, res: Response) { 
+export async function deleteRoute(req: Request, res: Response) {
     const route = await Route.findOneAndDelete(req.params);
     return res.json({
         message: 'Route deleted successfully'
