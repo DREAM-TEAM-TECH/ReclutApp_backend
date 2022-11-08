f9117f11ea9661488a804612b00bec82ce5fd58a
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-11-07 23:02:15 -0600

    Changing the points to an array of points

--- src/models/Route.ts
+++ src/models/Route.ts
@@ -3,9 +3,13 @@ import { Schema, model, Document, Types } from 'mongoose'
 const RouteSchema = new Schema({
     name: { type: String, required: true },
     //transportation
-    transportation: { type: Schema.Types.ObjectId, ref: "Transportation" , required: true},
+    transportation: { type: Schema.Types.ObjectId, ref: "Transportation", required: true },
     //points
-    points: { type: Schema.Types.ObjectId, ref: "Point" },
+    points: [{
+        id: {
+            type: Schema.Types.ObjectId, ref: "Point"
+        }
+    }],
 });
 
 interface IRoute extends Document {
