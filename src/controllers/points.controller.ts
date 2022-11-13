3ceaa10e384d085e015eec8ef4563ef8b92985aa
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-11-13 00:40:55 -0600

    Returning the ID of the new point

--- src/controllers/points.controller.ts
+++ src/controllers/points.controller.ts
@@ -1,31 +1,29 @@
 import { Request, Response } from 'express'
 import Point from '../models/Point'
 
-export async function createPoint(req: Request, res: Response) { 
-    const newPoint = await Point.create(req.body); 
-    return res.json({
-        message: 'Point created successfully'
-    });
+export async function createPoint(req: Request, res: Response) {
+    const newPoint = await Point.create(req.body);
+    return res.json({ id: newPoint._id });
 }
 
-export async function getPoints(req: Request, res: Response) { 
+export async function getPoints(req: Request, res: Response) {
     const points = await Point.find();
     return res.json(points);
 }
 
-export async function getPoint(req: Request, res: Response) { 
+export async function getPoint(req: Request, res: Response) {
     const point = await Point.findById(req.params.id);
     return res.json(point);
 }
 
-export async function updatePoint(req: Request, res: Response) { 
+export async function updatePoint(req: Request, res: Response) {
     let point = await Point.findOneAndUpdate(req.params, req.body);
     return res.json({
         message: 'Point updated successfully'
     });
 }
 
-export async function deletePoint(req: Request, res: Response) { 
+export async function deletePoint(req: Request, res: Response) {
     const point = await Point.findOneAndDelete(req.params);
     return res.json({
         message: 'Point deleted successfully'
