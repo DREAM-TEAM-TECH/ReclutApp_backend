79e6415ac9872c28500c2a41d2bb4e6228ff755f
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-11-18 00:44:51 -0600

    Populating only vacant and company in candidates

--- src/controllers/candidates.controller.ts
+++ src/controllers/candidates.controller.ts
@@ -4,7 +4,6 @@ import Candidate from '../models/Candidate'
 export async function createCandidate(req: Request, res: Response) {
     const newCandidate = await Candidate.create(req.body);
     const candidate = await Candidate.findById(newCandidate)
-        .populate('point')
         .populate('vacant')
         .populate('company')
     return res.json(candidate);
@@ -12,22 +11,22 @@ export async function createCandidate(req: Request, res: Response) {
 
 export async function getCandidates(req: Request, res: Response) {
     const candidates = await Candidate.find()
-        .populate('point')
         .populate('vacant')
         .populate('company')
     return res.json(candidates);
 }
 
 export async function getCandidatesByPoint(req: Request, res: Response) {
-    const candidates = await Candidate.find({ point: req.params.id });
+    const candidates = await Candidate.find({ point: req.params.id })
+        .populate('vacant')
+        .populate('company')
     return res.json(candidates);
 }
 
 export async function getCandidate(req: Request, res: Response) {
     const candidate = await Candidate.findById(req.params.id)
-        .populate('point')
-    // .populate('vacant')
-    // .populate('company')
+        .populate('vacant')
+        .populate('company')
     return res.json(candidate);
 }
 
