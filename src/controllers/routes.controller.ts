990b47d675d1db019357d0423015586d3f5ac29c
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-11-21 15:30:04 -0600

    Adding a new point to a route or updating the info

--- src/controllers/routes.controller.ts
+++ src/controllers/routes.controller.ts
@@ -25,12 +25,36 @@ export async function getRoute(req: Request, res: Response) {
 
 export async function updateRoute(req: Request, res: Response) {
 
-    const prevPoints = await Route.findById(req.params.id)
+    const prevRoute = await Route.findById(req.params.id)
 
-    // let route = await Route.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    //     .populate('transportation')
-    //     .populate('points')
-    return res.json({ p: prevPoints?.points });
+    if (!prevRoute) {
+        return res.status(400).json({
+            msg: "No existe una ruta con el id " + req.params.id
+        })
+    } else {
+
+        const { point, ...infoRoute } = req.body;
+
+        if (point) {
+
+            prevRoute.points.push(point);
+
+            const route = await Route.findByIdAndUpdate(req.params.id, prevRoute, { new: true })
+                .populate('transportation')
+                .populate('points');
+
+            return res.json(route);
+
+        } else {
+
+            const route = await Route.findByIdAndUpdate(req.params.id, infoRoute, { new: true })
+                .populate('transportation')
+                .populate('points');
+
+            return res.json(route);
+
+        }
+    }
 }
 
 export async function deleteRoute(req: Request, res: Response) {
