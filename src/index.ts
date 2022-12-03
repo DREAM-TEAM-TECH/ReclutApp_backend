d3feedef954d10ebd088690793963cc8fa75201f
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-12-03 17:24:49 -0600

    Fix: tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import app from './src/app'
-import { connection } from './src/database'
+import app from './app'
+import { connection } from './database'
 import schedule from 'node-schedule'
-import { deleteAllCandidates } from './src/controllers/candidates.controller'
+import { deleteAllCandidates } from './controllers/candidates.controller'
 import dotenv from 'dotenv'
 import cors from 'cors';
 
 async function main() {
     dotenv.config();
     await connection();
-    app.use(cors()); 
+    app.use(cors());
     app.listen(app.get('port'));
     console.log('Server on port', app.get('port'));
     const job = schedule.scheduleJob({ hour: 23, minute: 59 }, () => {
