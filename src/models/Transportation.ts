35c8f77da3d9cbfed61c7940b2c4b2b85b23685b
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-12-07 20:08:44 -0600

    Field name added in the model of Transportation

--- src/models/Transportation.ts
+++ src/models/Transportation.ts
@@ -3,12 +3,14 @@ import { Schema, model, Document } from 'mongoose'
 const TransportationSchema = new Schema({
     license_plates: { type: String, required: true },
     //photos
-    photos: [{ type: String }]
+    photos: [{ type: String }],
+    name: { type: String, required: true }
 });
 
 interface ITransportation extends Document {
     license_plates: string,
     photos: string[],
+    name: string
 };
 
 export default model<ITransportation>('Transportation', TransportationSchema);
